a3a1f98cf43d01eae64bb62e63d1938c033aaab2
dotbreaker88 <dotbreaker88@gmail.com>
2026-08-09 21:51:24 -0400

    Update runtime version to 0.6.11

--- scripts/candlelight.mjs
+++ scripts/candlelight.mjs
@@ -7,7 +7,7 @@ import {CandlelightRolls} from "./rolls.mjs";
 import {CandlelightLoot} from "./loot.mjs";
 
 Hooks.once("init",()=>{
-  console.log("Candlelight | Initializing v0.6.10");
+  console.log("Candlelight | Initializing v0.6.11");
   CONFIG.Actor.documentClass=CandlelightActor; CONFIG.Item.documentClass=CandlelightItem;
   CONFIG.Actor.dataModels={character:CharacterData,loot:LootData};
   CONFIG.Item.dataModels={weapon:WeaponData,armor:ArmorData,accessory:EquipmentData,equipment:EquipmentData,ability:AbilityData,talent:TalentData,heritage:HeritageData,theme:ThemeData,class:ClassData,spirit:SpiritData,element:ElementData,massiveWound:MassiveWoundData};
